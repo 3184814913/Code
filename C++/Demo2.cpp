981c20b8f8b99f4ff3a26d88036ddde6a3ff2e4c
Dreamsheep <3184814913@qq.com>
2023-09-24 14:41:05 +0800

    Demo

--- C++/Demo2.cpp
+++ C++/Demo2.cpp
@@ -3,15 +3,20 @@ long get_long(void);
 
 int main(){
 
+    int demo = get_long();
+    printf("%d",demo);
 }
 
 long get_long(void){
     long input;
-    char;
+    char ch;
     while(scanf("%d",&input) != 1)
     {
         while((ch = getchar()) != '\n')
+            putchar(ch);
+        printf("is not an integer.");
     }
+    return input;
 }
 
 
